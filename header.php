929714974bda3b86665eafa4f389b4ea445f4b27
Valentin Grenier <valentin@Mac-de-Valentin.lan>
2024-05-26 09:37:31 +0200

    Removed st-page class on body

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
 
 <body <?php body_class(); ?>>
 	<?php wp_body_open(); ?>
-	<div id="page" class="st-page">
+	<div id="page">
 
 		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Aller au contenu', 'studio-val'); ?></a>
 
