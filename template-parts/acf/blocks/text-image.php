e5ee9ea4fe59ee0a3e152173defc3a20fe6b74d0
Valentin Grenier <valentin.grenier02@gmail.com>
2024-05-27 11:55:29 +0200

    Added text image block

---
+++ template-parts/acf/blocks/text-image.php
@@ -0,0 +1,20 @@
+<?php
+// === ACF Data
+$image = get_sub_field('image');
+$text = get_sub_field('text');
+$content_order = get_sub_field('content_order');
+?>
+
+<div class="st-text-image <?php echo esc_attr($content_order); ?>">
+  <?php if ($image) : ?>
+    <div class="st-text-image__image">
+      <img src="<?php echo esc_url($image['url']) ?>" alt="<?php echo esc_url($image['alt']) ?>">
+    </div>
+  <?php endif; ?>
+
+  <?php if ($text) : ?>
+    <div class="st-text-image__text">
+      <?php echo $text; ?>
+    </div>
+  <?php endif; ?>
+</div>
