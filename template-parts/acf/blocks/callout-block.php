34e77e3d4fdf34d6f4177c48f1d06f9f2322ccfa
Valentin Grenier <valentin.grenier02@gmail.com>
2024-05-27 12:25:30 +0200

    Added callout block

---
+++ template-parts/acf/blocks/callout-block.php
@@ -0,0 +1,31 @@
+<?php
+// === ACF Data
+$content = get_sub_field('content');
+$number_of_buttons = get_sub_field('number_of_buttons');
+$buttons = [
+  $button_1 = get_sub_field('button_1')
+];
+if ($number_of_buttons > 1) {
+  $button_2 = get_sub_field('button_2');
+  $buttons[] = $button_2;
+}
+
+?>
+
+<div class="st-callout-block">
+  <?php if ($content) : ?>
+    <div class="st-callout-block__content">
+      <?php echo $content; ?>
+    </div>
+  <?php endif; ?>
+
+  <div class="st-callout-block__buttons">
+    <?php if ($buttons) : ?>
+      <?php foreach ($buttons as $button) : ?>
+        <?php if ($button !== null) : ?>
+          <?php get_template_part('template-parts/button', null, $button); ?>
+        <?php endif; ?>
+      <?php endforeach; ?>
+    <?php endif; ?>
+  </div>
+</div>
