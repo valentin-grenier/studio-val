26773cef4c605fcef50113732d2427f0a18b6f6e
Valentin Grenier <valentin.grenier02@gmail.com>
2024-05-29 11:49:08 +0200

    Added text-width variable to block

--- template-parts/acf/blocks/text.php
+++ template-parts/acf/blocks/text.php
@@ -1,8 +1,24 @@
 <?php
 // === ACF Data
 $text = get_sub_field('text_content');
+$text_width = get_sub_field('text_width');
+
+switch ($text_width) {
+  case "100":
+  default:
+    $width = "width-100";
+    break;
+
+  case "75":
+    $width = "width-75";
+    break;
+
+  case "50":
+    $width = "width-50";
+    break;
+}
 ?>
 
-<div class="st-text">
+<div class="st-text <?php echo esc_attr($width); ?>">
   <?php echo $text; ?>
 </div>
