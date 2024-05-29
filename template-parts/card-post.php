bba16dabed04469cf4c35867f1309e70531aef5d
Valentin Grenier <valentin.grenier02@gmail.com>
2024-05-29 10:58:00 +0200

    Card post: edited image height and title font-size

--- template-parts/card-post.php
+++ template-parts/card-post.php
@@ -5,10 +5,11 @@ $excerpt = get_the_excerpt();
 $category = get_the_category(get_the_ID());
 $thumbnail_url = get_the_post_thumbnail_url();
 $thumbnail_alt = get_the_post_thumbnail_alt(get_the_ID());
+$permalink = get_permalink();
 
 ?>
 
-<div class="st-card-post <?php echo $is_slider ? "swiper-slide" : ""; ?>">
+<a class="st-card-post <?php echo $is_slider ? "swiper-slide" : ""; ?>" href="<?php echo esc_url($permalink); ?>">
   <div class="st-card-post__image">
     <?php if ($thumbnail_url) : ?>
       <img src="<?php echo esc_url($thumbnail_url); ?>" alt="<?php echo esc_attr($thumbnail_alt); ?>">
@@ -19,4 +20,4 @@ $thumbnail_alt = get_the_post_thumbnail_alt(get_the_ID());
     <h3><?php echo $title; ?></h3>
     <p><?php echo $excerpt; ?></p>
   </div>
-</div>
+</a>
