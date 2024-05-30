810fe252271e485772f87455399dff37ce73c537
Valentin Grenier <valentin.grenier02@gmail.com>
2024-05-30 11:37:38 +0200

    Edited review-list block to display a slider or a grid

--- template-parts/acf/blocks/reviews-list.php
+++ template-parts/acf/blocks/reviews-list.php
@@ -1,6 +1,7 @@
 <?php
 // === ACF Data
 $number_of_reviews = get_sub_field('number_of_reviews');
+$layout = get_sub_field('layout');
 
 // === Retrieve reviews
 $reviews_args = array(
@@ -12,19 +13,33 @@ $reviews_args = array(
 $reviews_query = new WP_Query($reviews_args);
 ?>
 
-<div class="st-reviews-list swiper">
-  <div class="swiper-wrapper">
-    <?php if ($reviews_query->have_posts()) : ?>
-      <?php while ($reviews_query->have_posts()) : $reviews_query->the_post(); ?>
-        <?php get_template_part('template-parts/card', 'review'); ?>
-      <?php wp_reset_postdata();
-      endwhile ?>
-    <?php endif; ?>
+<?php if ($layout === "slider") : ?>
+  <div class="st-reviews-list swiper">
+    <div class="st-reviews-list__slider swiper-wrapper">
+      <?php if ($reviews_query->have_posts()) : ?>
+        <?php while ($reviews_query->have_posts()) : $reviews_query->the_post(); ?>
+          <?php get_template_part('template-parts/card', 'review'); ?>
+        <?php wp_reset_postdata();
+        endwhile ?>
+      <?php endif; ?>
 
-  </div>
+    </div>
 
-  <div class="swiper-buttons">
-    <div class="swiper-button-prev"></div>
-    <div class="swiper-button-next"></div>
+    <div class="swiper-buttons">
+      <div class="swiper-button-prev"></div>
+      <div class="swiper-button-next"></div>
+    </div>
   </div>
-</div>
+<?php else : ?>
+  <div class="st-reviews-list">
+    <div class="st-reviews-list__grid">
+      <?php if ($reviews_query->have_posts()) : ?>
+        <?php while ($reviews_query->have_posts()) : $reviews_query->the_post(); ?>
+          <?php get_template_part('template-parts/card', 'review'); ?>
+        <?php wp_reset_postdata();
+        endwhile ?>
+      <?php endif; ?>
+
+    </div>
+  </div>
+<?php endif; ?>
