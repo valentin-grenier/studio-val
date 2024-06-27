4b1d500394e41f83552408063b5045e0def06e8d
Valentin <valentin.grenier02@gmail.com>
2024-06-27 22:25:41 +0200

    Prepared contact page fields and page template (without html)

--- functions.php
+++ functions.php
@@ -98,7 +98,7 @@ function studio_disable_block_editor($use_block_editor, $post_type)
 	// = Get current page template
 	$template = basename(get_page_template());
 
-	if ('page' === $post_type && ($template === "template-acf.php" || $template === "template-maintenance-form.php")) {
+	if ('page' === $post_type && ($template === "template-acf.php" || $template === "template-maintenance-form.php" || $template === "template-contact-page.php")) {
 		return false;
 	}
 	return $use_block_editor;
---
+++ template-contact-page.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Template Name: Contact page
+ * The dedicated page template for maintenance subscription
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Studio Val
+ */
+
+get_header();
+?>
+
+<main id="content" class="st-template-contact-page">
+
+
+</main>
+
+<?php
+get_footer();
