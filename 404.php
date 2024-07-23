3bb548a24ffab155b23149c358565a6fd6af33e9
Valentin <valentin.grenier02@gmail.com>
2024-07-23 14:43:01 +0200

    Added custom 404 page

--- 404.php
+++ 404.php
@@ -11,9 +11,24 @@
 get_header();
 ?>
 
-<main id="content" class="site-main">
+<main id="content" class="site-main st-404">
+    <section class="st-section boxed">
+        <dotlottie-player src="https://lottie.host/c2aa10e1-ffe6-41ca-939f-c28f80b964a1/nojiMNQLMG.json" background="transparent" speed="1" style="width: 275px; height: 275px;" loop autoplay></dotlottie-player>
 
+
+        <h1><?php _e("Oh non !<br /> <strong>C'est cassé !</strong> ", "studio-val"); ?></h1>
+
+        <p><?php _e("Il semblerait que Philou ait tout débranché. <br />Sinon, la page que vous recherchez n'existe pas.", "studio-val"); ?></p>
+
+        <div class="st-button primary">
+            <a href="<?php echo esc_url(home_url('/')); ?>"><?php _e("Retour à l'accueil", "studio-val"); ?></a>
+        </div>
+    </section>
 </main>
 
+
+
+<script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module" defer></script>
 <?php
+
 get_footer();
