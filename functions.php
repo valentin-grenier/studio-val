2085bff48eb54387924d24531900468ff6269546
Valentin <valentin.grenier02@gmail.com>
2024-09-02 15:35:23 +0200

    Single: Added styles for list block

--- functions.php
+++ functions.php
@@ -103,6 +103,9 @@ function studio_scripts()
 	if (is_singular() && comments_open() && get_option('thread_comments')) {
 		wp_enqueue_script('comment-reply');
 	}
+
+	// == Block variations
+	wp_enqueue_script('studio-block-variations', get_template_directory_uri() . '/assets/js/block-variations.js', array('wp-blocks', 'wp-dom', 'wp-edit-post'));
 }
 add_action('wp_enqueue_scripts', 'studio_scripts');
 
