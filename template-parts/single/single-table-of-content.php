c01e7b6a49c3cdd28f7998b7dc3b7ca517f57d86
Valentin <valentin.grenier02@gmail.com>
2024-09-02 17:15:10 +0200

    Fixed: script did not include removing apostrophes from string

--- template-parts/single/single-table-of-content.php
+++ template-parts/single/single-table-of-content.php
@@ -12,9 +12,10 @@ preg_match_all('/<h([2-3]).*?>(.*?)<\/h[2-3]>/si', get_the_content(), $matches, 
         <?php
         $heading_level = $match[1];
         $heading_text = strip_tags($match[2]);
-        $heading_id = transliterator_transliterate('Any-Latin; Latin-ASCII', $heading_text);
-        $heading_id = strtolower(preg_replace('/[^a-zA-Z0-9]+/', '-', $heading_id));
-        $heading_id = rtrim($heading_id, '-');
+        $heading_id = transliterator_transliterate('Any-Latin; Latin-ASCII', $heading_text); // Convert accents to non accents characters
+        $heading_id = str_replace("'", '', $heading_id); // Remove apostrophes
+        $heading_id = strtolower(preg_replace('/[^a-zA-Z0-9]+/', '-', $heading_id)); // Replace non-alphanumeric characters with hyphens
+        $heading_id = rtrim($heading_id, '-'); // Remove trailing hyphens
         ?>
         <li class="<?php echo esc_attr('h' . $heading_level); ?>">
           <a href=" #<?php echo esc_attr($heading_id); ?>">
