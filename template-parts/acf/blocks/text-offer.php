55f003c91b0b3ee7690def478e2cf63848e34faa
Valentin <valentin.grenier02@gmail.com>
2024-09-08 14:44:53 +0200

    Edited text offer component with new fields values

--- template-parts/acf/blocks/text-offer.php
+++ template-parts/acf/blocks/text-offer.php
@@ -1,9 +1,16 @@
 <?php
 $text = get_sub_field('text');
 $offer = get_sub_field('offer');
-$services = $offer['services'];
-$button = $offer['button'];
-$checkmark_icon = file_get_contents(get_template_directory_uri() . '/assets/svg/checkmark.svg');
+
+if ($offer) {
+    $offer_post_id = $offer->ID;
+    $card_content = get_field('card_content', $offer_post_id);
+    $included_services = get_field('included_services', $offer_post_id);
+    $button = get_field('button', $offer_post_id);
+
+    $checkmark_icon = file_get_contents(get_template_directory_uri() . '/assets/svg/checkmark.svg');
+    $cross_icon = file_get_contents(get_template_directory_uri() . '/assets/svg/cross.svg');
+}
 ?>
 
 <div class="st-text-offer">
@@ -11,27 +18,30 @@ $checkmark_icon = file_get_contents(get_template_directory_uri() . '/assets/svg/
         <p><?php echo $text; ?></p>
     </div>
 
-    <div class="st-text-offer__offer">
-        <div class="st-text-offer__offer--title">
-            <h3><strong><?php _e('Audit Technique WordPress', 'studio-val'); ?></strong> - 99€ HT</h3>
-            <p><?php _e('Rapport d’analyse détaillé sur les performances, la sécurité, le responsive et le SEO de votre site WordPress.', 'studio-val'); ?></p>
-        </div>
-        <div class="st-text-offer__offer--list">
-            <?php if ($offer): ?>
-                <ul>
-                    <?php foreach ($services as $service): ?>
-                        <li>
-                            <?php echo $checkmark_icon; ?>
-                            <span><?php echo $service['title']; ?></span>
-                            <p><?php echo $service['text']; ?></p>
-                        </li>
-                    <?php endforeach; ?>
-                </ul>
-            <?php endif; ?>
+    <?php if ($offer): ?>
+        <div class="st-text-offer__offer">
+            <div class="st-text-offer__offer--title">
+                <h3><?php echo $card_content['title']; ?></h3>
+                <span><?php echo $card_content['price']; ?></span>
+                <p><?php echo $card_content['subtitle']; ?></p>
+            </div>
+            <div class="st-text-offer__offer--list">
+                <?php if (!empty($included_services)): ?>
+                    <ul>
+                        <?php foreach ($included_services as $service): ?>
+                            <li>
+                                <?php echo $service['is_included'] ? $checkmark_icon : $cross_icon; ?>
+                                <span><?php echo $service['title']; ?></span>
+                                <p><?php echo $service['subtitle']; ?></p>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php endif; ?>
 
-            <?php if ($button) : ?>
-                <?php get_template_part('template-parts/button', null, $button); ?>
-            <?php endif; ?>
+                <?php if ($button) : ?>
+                    <?php get_template_part('template-parts/button', null, $button); ?>
+                <?php endif; ?>
+            </div>
         </div>
-    </div>
+    <?php endif; ?>
 </div>
